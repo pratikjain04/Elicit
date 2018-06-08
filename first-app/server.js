8833999b63ad2764a34eb79710fdd2de709cefbd
vidhyanshu <v.vidu171@gmail.com>
2018-06-08 13:16:39 +0530

    Login Page

--- first-app/server.js
+++ first-app/server.js
@@ -1,21 +1,42 @@
-var express = require('express')
-var logger = require('morgan')
-var app = express()
+var     express = require('express'),
+        logger = require('morgan'),
+        app = express(),
+        bodyParser = require('body-parser');
+
+// var firebase = require('firebase');
 
 
+app.use(require("express-session")({        
+        secret: "iloveprogramming",
+        resave: false,
+        saveUninitialized: false
+    }));
 
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 app.use(express.static('views'))
 app.use(logger('dev'))
 app.set('views', __dirname + '/views')
 
+
+app.use(bodyParser.urlencoded({extended : true}));
+
 app.get('/',function(request,response){
        // response.send("<h1>First App</h1>")
           response.render('home.ejs')
-        
 })
 
-var port = process.env.PORT
+
+app.get("/home", (req, res)=>{    
+        res.sendFile(__dirname+"/index.html");
+        // res.render("index.ejs");
+})
+
+app.post('/register', (req, res)=>    {
+        console.log(req.body); 
+    });
+
+var port = process.env.PORT || 8001
 
 app.listen(port, function(){
         console.log('App Running on Port ' +  port)
