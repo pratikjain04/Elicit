93ccb2c036c47e5b88578093a0ab0de9ae7605ed
vidhyanshu <v.vidu171@gmail.com>
2018-06-25 15:58:06 +0530

    changes

--- first-app/server.js
+++ first-app/server.js
@@ -3,8 +3,19 @@ var     express = require('express'),
         app = express(),
         bodyParser = require('body-parser');
 
-// var firebase = require('firebase');
+var firebase = require('firebase');
 
+var config = {
+        apiKey: "AIzaSyBvgALqhClKBVDF7-1gyumStnBhPhF75ws",
+        authDomain: "login-page-cf50a.firebaseapp.com",
+        databaseURL: "https://login-page-cf50a.firebaseio.com",
+        projectId: "login-page-cf50a",
+        storageBucket: "login-page-cf50a.appspot.com",
+        messagingSenderId: "470107849478"
+      };
+    
+      firebase.initializeApp(config);
+      
 
 app.use(require("express-session")({        
         secret: "iloveprogramming",
@@ -21,19 +32,43 @@ app.set('views', __dirname + '/views')
 
 app.use(bodyParser.urlencoded({extended : true}));
 
+function isLoggedin(req, res, next){
+    
+        if(req.isAuthenticated()){
+            return next(); 
+        }
+        res.redirect("/login");
+}
+
+
 app.get('/',function(request,response){
        // response.send("<h1>First App</h1>")
           response.render('home.ejs')
 })
 
 
+app.get('/login',function(request,response){
+        // response.send("<h1>First App</h1>")
+           response.render('home.ejs')
+ })
+
 app.get("/home", (req, res)=>{    
-        res.sendFile(__dirname+"/index.html");
+        // res.sendFile(__dirname+"/index1.html");
+        // console.log("user id: " + firebase.auth().currentUser.uid);
         // res.render("index.ejs");
 })
 
 app.post('/register', (req, res)=>    {
         console.log(req.body); 
+        firebase.auth().createUserWithEmailAndPassword(req.body.username, req.body.password).catch(function(error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorMessage);
+                
+                // ...
+              });
+        console.log(firebase.auth().currentUser);
     });
 
 var port = process.env.PORT || 8001
@@ -41,3 +76,9 @@ var port = process.env.PORT || 8001
 app.listen(port, function(){
         console.log('App Running on Port ' +  port)
 })
+
+app.get("*", function(req, res){
+        res.send("<strong>404</strong> You seem lost!");
+    });
+
+    
